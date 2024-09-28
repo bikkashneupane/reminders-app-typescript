015dca395dcfe8989f30c3354220b584a77b7c9e
bikashneupane17 <edubikkash@gmail.com>
2024-09-28 22:12:36 +1000

    Reminder Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import ReminderList from "./component/ReminderList";
+import Reminder from "./interface/reminder";
+
+const items: Reminder[] = [
+  { id: 1, title: "Reminder 1" },
+  { id: 2, title: "Reminder 2" },
+];
 
 function App() {
-  return <>Typescript Basic</>;
+  return (
+    <>
+      <ReminderList items={items} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/component/ReminderList.tsx
@@ -0,0 +1,16 @@
+import Reminder from "../interface/reminder";
+
+interface ReminderListProps {
+  items: Reminder[];
+}
+function ReminderList({ items }: ReminderListProps) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ReminderList;
